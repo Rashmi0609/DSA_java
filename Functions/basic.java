befb541ec3b0e7d365dce0b91eb1c49554fe3324
Rashmi0609 <rashmi63867@gmail.com>
2025-07-30 23:25:28 +0530

    functions

---
+++ Functions/basic.java
@@ -0,0 +1,9 @@
+public class basic {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = add(1, 2); 
+        System.out.println("The sum is: " + result); 
+    }
+}
