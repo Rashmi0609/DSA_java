cac008f4f76fd6787a4fc4ea9aa844e725548dd5
Rashmi0609 <rashmi63867@gmail.com>
2025-08-01 21:48:15 +0530

    swapping

--- Functions/swap.java
+++ Functions/swap.java
@@ -1,3 +1,15 @@
 public class swap {
-    
+
+    public static void Swap(int a, int b) {
+        int temp= a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        Swap(a,b);
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+    }
 }
