e4e323a1cfc6015128f2d901d00259ddbe2a565f
Rashmi0609 <rashmi63867@gmail.com>
2025-08-05 23:51:58 +0530

    primeno.

---
+++ Functions/primeORnot.java
@@ -0,0 +1,23 @@
+public class primeORnot {
+    //corner cases
+    //2
+    public static boolean isPrime(int n){
+        if(n==2){
+            return true;
+        }
+        boolean isPrime = true;
+        for(int i=2 ; i<=n-1 ;i++){
+            if(n%i==0){
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(7));
+        System.out.println(isPrime(10));
+    }    
+}
