9902f79006a3c857d434efd8de082b40b07ada7d
Rashmi0609 <rashmi63867@gmail.com>
2026-06-14 12:18:36 +0530

    fibonacci Number

---
+++ Array/fibonacci.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        if(n>=1){
+            arr[0]=0;
+        }
+        if(n>=2){
+            arr[1]=1;
+        }
+        for(int i=2;i<n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();   
+    } 
+     
+}
